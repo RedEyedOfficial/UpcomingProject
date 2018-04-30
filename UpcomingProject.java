19f3677f870152794a5a15c2698a28c9dfe73508
Zomaru-kun <renaldypurwanto@gmail.com>
2018-04-30 19:17:45 +0700

    Something is moving underground !!

---
+++ UpcomingProject.java
@@ -0,0 +1,16 @@
+public class UpcomingProject {
+	public String yi;
+	public String kalih;
+	public String san;
+	public String four;
+	public String lima;
+	
+	public void ZeroTwoisTheBestgirl() {
+		yi = "Carefully";
+		kalih = "Feel";
+		san = "Friends";
+		four = "We";
+		lima = "Like";
+		System.out.print("test");
+		}
+}
