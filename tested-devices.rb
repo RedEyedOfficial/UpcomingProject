4113238d4dd610db4e16663b125079c0bf8b207a
zomaru <renaldypurwanto@gmail.com>
2018-05-14 18:43:41 +0700

    List of tested devices

---
+++ tested-devices.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'colorize'
+
+puts "#{'Zomaru'.cyan} <=> #{'Zero Two'.green} <=> #{'Wallpaper'.yellow}"
+puts "This app has been tested in my own devices;"
+puts''
+puts "#{'00000'.red}  #{'00000'.green}  #{'22222'.blue}"
+puts "#{'00 00'.red}  #{'00 00'.green}  #{'2 22'.blue}"
+puts "#{'00 00'.red}  #{'00 00'.green}   #{'22'.blue}"
+puts "#{'00 00'.red}  #{'00 00'.green}  #{'22 22'.blue}"
+puts "#{'00000'.red}  #{'00000'.green}  #{'22222'.blue}"
+puts''
+puts "#{'Andromax A16C3H'.blue}"
+puts "#{'Xiaomi Redmi Note 3 Pro'.green}"
+puts "#{'OnePlus A3010'.red}"
+
+# Zero Two is lovable, alive, and thus the best girl
